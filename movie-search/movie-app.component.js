99dca94495e7bc48f140316388f8c3af7ae7aebb
makrelito <makrelito@yahoo.fr>
2016-05-19 16:41:08 +0200

    call to json API omdb

--- movie-search/movie-app.component.js
+++ movie-search/movie-app.component.js
@@ -8,14 +8,29 @@
       info.name = "toto";
     };
     function homeSearch($http){
-      var homesearch= this;
-      homesearch.msg = "homie";
-      var url = "http://www.omdbapi.com/?type=movie&t=constantine&y=2005&plot=short&tomatoes=true&r=json";
-      //$http.get();
+      var homesearch = this;
+      homesearch.$onInit = function(){
+        homesearch.msg = "";
+        var type = homesearch.type;
+        type = "type of search";
+        homesearch.typeList = [
+          {"id" : 1,"name": "Movie"},
+          {"id" : 2,"name": "Serie"},
+          {"id" : 3,"name": "Episode"}
+        ];
+        homesearch.lookingFor = function(){
+          var url = "http://www.omdbapi.com/?type="+homesearch.type.name+"&t="+homesearch.search+"&tomatoes=true&r=json";
+          $http.get(url, function(success){
+            console.log(success);
+          })
+        };
+      }
+
+      //;
     };
     module.component("homeMovie", {
       templateUrl:"movie-search/movie-home.component.html",
-      controller: ['$http',homeSearch],
+      controller: ['$http', homeSearch],
       controllerAs: 'homesearch'
     });
     module.component("movieDetails", {
